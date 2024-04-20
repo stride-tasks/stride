55ee23abadd761c70087f6f61cc0f8179e13ada1
Haled Odat <8566042+HalidOdat@users.noreply.github.com>
2024-04-20 19:15:12 +0200

    Remove connection test from app bar

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -4,7 +4,6 @@ import 'package:stride/blocs/settings_bloc.dart';
 import 'package:stride/blocs/tasks_bloc.dart';
 import 'package:stride/routes/routes.dart';
 import 'package:stride/src/rust/api/repository.dart';
-import 'package:stride/src/rust/git/known_hosts.dart';
 
 class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
   final String title;
@@ -31,7 +30,6 @@ class _CustomAppBarState extends State<CustomAppBar> {
   Widget build(BuildContext context) {
     return BlocBuilder<SettingsBloc, SettingsState>(
       builder: (context, state) {
-        final settings = state.settings;
         return AppBar(
           title: Text(
             widget.title,
@@ -57,94 +55,6 @@ class _CustomAppBarState extends State<CustomAppBar> {
                   context.read<TaskBloc>().add(TaskFetchEvent());
                 }
               },
-            ),
-            IconButton(
-              icon: FutureBuilder(
-                future: sync,
-                builder: (context, snapshot) {
-                  print(snapshot);
-                  if (snapshot.hasError && !seenError) {
-                    if (snapshot.error! is ConnectionError_UnknownHost) {
-                      final error =
-                          snapshot.error! as ConnectionError_UnknownHost;
-
-                      final snackBar = SnackBar(
-                        content: Text('Unknown host ${error.hostname}'),
-                        action: SnackBarAction(
-                          label: "Add",
-                          onPressed: () {
-                            context.read<SettingsBloc>().add(
-                                  SettingsAddKnownHostEvent(
-                                    host: Host(
-                                      hostname: error.hostname,
-                                      remoteKeyType: error.keyType,
-                                      remoteHostKey: error.hostKey,
-                                    ),
-                                  ),
-                                );
-                          },
-                        ),
-                        dismissDirection: DismissDirection.endToStart,
-                        duration: const Duration(days: 1),
-                      );
-
-                      seenError = true;
-
-                      WidgetsBinding.instance.addPostFrameCallback((_) {
-                        ScaffoldMessenger.of(context)
-                          ..hideCurrentSnackBar()
-                          ..showSnackBar(snackBar);
-                      });
-                    }
-
-                    return const Icon(Icons.perm_scan_wifi_rounded);
-                  }
-                  switch (snapshot.connectionState) {
-                    case ConnectionState.none:
-                      return const Icon(Icons.wifi_rounded);
-                    case ConnectionState.waiting:
-                    case ConnectionState.active:
-                      return const CircularProgressIndicator();
-                    case ConnectionState.done:
-                      return const Icon(Icons.check);
-                  }
-                },
-              ),
-              onPressed: () async {
-                if (settings.repository.sshKeyUuid == null) {
-                  return await showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        content: const Text(
-                          "The repository does not have an assigned SSH key",
-                        ),
-                        actions: [
-                          IconButton(
-                            icon: const Icon(Icons.cancel),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                }
-                final sshKey = state.settings.keys.firstWhere(
-                  (element) => element.uuid == settings.repository.sshKeyUuid,
-                );
-
-                setState(() {
-                  seenError = false;
-                  sync = testConnection(
-                    url: settings.repository.origin,
-                    publicKey: sshKey.public,
-                    privateKey: sshKey.private,
-                  );
-                });
-              },
-              color: Theme.of(context).secondaryHeaderColor,
             ),
             IconButton(
               icon: const Icon(Icons.settings),
