be95dce58c1d368585ed0d32a4c93c9a6e5d244d
Haled Odat <8566042+HalidOdat@users.noreply.github.com>
2024-08-08 21:13:17 +0200

    Integration tes init repository in flutter bridge

---
+++ crates/flutter_bridge/tests/repository_init.rs
@@ -0,0 +1,26 @@
+#![allow(unused_crate_dependencies)]
+
+mod common;
+
+use common::*;
+use stride_flutter_bridge::api::repository::TaskStorage;
+
+#[test]
+fn init_repository() -> anyhow::Result<()> {
+    let mut fixture = setup("init_repository")?;
+
+    let mut storage = TaskStorage::new(&fixture.support_dir.join("repository").to_string_lossy());
+
+    let commits = storage.log(None, None)?;
+    assert!(commits.is_none());
+
+    storage.init_repotitory()?;
+
+    let commits = storage.log(None, None)?;
+    let commits = commits.unwrap();
+
+    assert_eq!(commits[0].message.as_ref(), "Initial Commit");
+
+    fixture.cleanup();
+    Ok(())
+}
