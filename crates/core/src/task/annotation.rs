5ef9bcf3d29498e371cb25b413d47f7fd1aa569c
Haled Odat <8566042+HalidOdat@users.noreply.github.com>
2025-03-09 22:53:22 +0100

    Add constructor function for Annotation

--- crates/core/src/task/annotation.rs
+++ crates/core/src/task/annotation.rs
@@ -1,3 +1,4 @@
+use chrono::Utc;
 use serde::{Deserialize, Serialize};
 
 use super::Date;
@@ -6,4 +7,15 @@ use super::Date;
 pub struct Annotation {
     pub entry: Date,
     pub description: String,
+}
+
+impl Annotation {
+    /// Construct annotation with now as the entry date.
+    #[must_use]
+    pub fn now(description: String) -> Self {
+        Self {
+            entry: Utc::now(),
+            description,
+        }
+    }
 }
