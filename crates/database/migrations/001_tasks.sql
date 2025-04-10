0ac6d89f447fe0f7bbeafa3fb2784d4dd8f4b2e5
Haled Odat <8566042+HalidOdat@users.noreply.github.com>
2025-04-10 18:48:50 +0200

    Simplify sql

--- crates/database/migrations/001_tasks.sql
+++ crates/database/migrations/001_tasks.sql
@@ -1,92 +1,74 @@
 -- SQL: up
 
-CREATE TABLE status_table (
-    status_id INTEGER PRIMARY KEY,
-    status_name TEXT NOT NULL UNIQUE
+-- NOTE: The ID column of most of tables has to be globally unique,
+--       because the application is intented to be used in a distributed way,
+---      where the clients don't know about each other.
+
+CREATE TABLE IF NOT EXISTS task_table (
+    "id" BLOB PRIMARY KEY, -- UUID
+    "title" TEXT NOT NULL,
+    "entry" INTEGER NOT NULL,
+    "status" INTEGER NOT NULL DEFAULT 0, -- Default: pending
+    "priority" INTEGER,
+    "project" TEXT,
+    "modified" INTEGER,
+    "due" INTEGER,
+    "wait" INTEGER,
+
+    FOREIGN KEY (project) REFERENCES project_table (id) ON DELETE SET NULL
 ) STRICT;
 
-CREATE TABLE priority_table (
-    priority_id INTEGER PRIMARY KEY,
-    priority_name TEXT NOT NULL UNIQUE,
-    priority_urgency REAL NOT NULL DEFAULT 0.0
+CREATE TABLE IF NOT EXISTS task_dependency_table (
+    parent_task_id BLOB NOT NULL,
+    child_task_id BLOB NOT NULL,
+
+    FOREIGN KEY (parent_task_id) REFERENCES task_table (id) ON DELETE CASCADE,
+    FOREIGN KEY (child_task_id) REFERENCES task_table (id) ON DELETE CASCADE
 ) STRICT;
 
-CREATE TABLE project_table (
-    project_id INTEGER PRIMARY KEY,
-    project_name TEXT NOT NULL UNIQUE,
-    project_urgency REAL NOT NULL DEFAULT 0.0
+CREATE TABLE IF NOT EXISTS project_table (
+    id TEXT PRIMARY KEY CHECK (length(id) != 0)
 ) STRICT;
 
-CREATE TABLE task_table (
-    task_id BLOB PRIMARY KEY CHECK (length(task_id) = 16),
-    task_title TEXT NOT NULL,
-    task_entry INTEGER NOT NULL DEFAULT (unixepoch('now')),
-    task_status_id INTEGER NOT NULL DEFAULT 0,
-    task_priority_id INTEGER,
-    task_project_id INTEGER,
-    task_modified INTEGER,
-    task_due INTEGER,
-    task_wait INTEGER,
-    task_annotations BLOB NOT NULL DEFAULT (empty_blob()),
-    task_depends BLOB NOT NULL DEFAULT (empty_blob())
-    CHECK (mod(length(task_depends), 16) = 0),
-
-    FOREIGN KEY (task_status_id) REFERENCES status_table (status_id)
-    ON DELETE RESTRICT,
-
-    FOREIGN KEY (task_priority_id) REFERENCES priority_table (priority_id)
-    ON DELETE SET NULL,
-
-    FOREIGN KEY (task_project_id) REFERENCES project_table (project_id)
-    ON DELETE SET NULL
+CREATE TABLE IF NOT EXISTS tag_table (
+    id TEXT PRIMARY KEY CHECK (length(id) != 0)
 ) STRICT;
 
-CREATE TABLE uda_table (
-    uda_id INTEGER PRIMARY KEY,
-    uda_task_id BLOB NOT NULL,
-    uda_namespace TEXT NOT NULL DEFAULT '',
-    uda_key TEXT NOT NULL,
-    uda_value TEXT NOT NULL DEFAULT '',
-
-    FOREIGN KEY (uda_task_id) REFERENCES task_table (task_id) ON DELETE CASCADE
-) STRICT;
-
-CREATE TABLE tag_table (
-    tag_id INTEGER PRIMARY KEY,
-    tag_name TEXT NOT NULL,
-    tag_urgency REAL NOT NULL DEFAULT 0.0
-) STRICT;
-
-CREATE TABLE task_tag_table (
+CREATE TABLE IF NOT EXISTS task_tag_table (
     task_id BLOB NOT NULL,
-    tag_id INTEGER NOT NULL,
+    tag_id TEXT NOT NULL,
 
     PRIMARY KEY (task_id, tag_id),
 
-    FOREIGN KEY (task_id) REFERENCES task_table (task_id),
-    FOREIGN KEY (tag_id) REFERENCES tag_table (tag_id)
+    FOREIGN KEY (task_id) REFERENCES task_table (id) ON DELETE CASCADE,
+    FOREIGN KEY (tag_id) REFERENCES tag_table (id) ON DELETE CASCADE
 ) STRICT;
 
--- Populate with default data
+CREATE TABLE IF NOT EXISTS annotation_table (
+    "id" INTEGER PRIMARY KEY,
+    "entry" INTEGER NOT NULL,
+    "text" TEXT NOT NULL,
+    "task_id" BLOB NOT NULL,
 
-INSERT INTO status_table (status_id, status_name)
-VALUES
-(0, 'pending'),
-(1, 'complete'),
-(2, 'delete');
+    FOREIGN KEY (task_id) REFERENCES task_table (id) ON DELETE CASCADE
+) STRICT;
 
-INSERT INTO priority_table (priority_id, priority_name, priority_urgency)
-VALUES
-(0, 'low', -3.0),
-(1, 'medium', 3.0),
-(2, 'high', 6.0);
+CREATE TABLE IF NOT EXISTS uda_table (
+    "id" INTEGER PRIMARY KEY,
+    "namespace" TEXT,
+    "key" TEXT NOT NULL,
+    "value" BLOB,
+    "task_id" BLOB NOT NULL,
+
+    FOREIGN KEY (task_id) REFERENCES task_table (id) ON DELETE CASCADE
+) STRICT;
 
 -- SQL: down
 
+DROP TABLE IF EXISTS task_dependency_table;
+DROP TABLE IF EXISTS annotation_table;
 DROP TABLE IF EXISTS uda_table;
 DROP TABLE IF EXISTS task_tag_table;
 DROP TABLE IF EXISTS tag_table;
 DROP TABLE IF EXISTS task_table;
-DROP TABLE IF EXISTS status_table;
-DROP TABLE IF EXISTS priority_table;
 DROP TABLE IF EXISTS project_table;
